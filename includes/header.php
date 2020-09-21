4016a64030f8346ad74adb546da6b998b5bbe2d6
Florentino Navarro <tino.navarro@hotmail.com>
2020-09-21 15:43:57 -0500

    first commit

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php 
+  require_once('variables.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $title; ?></title>
+</head>
+<body>
+  
+</body>
+</html>
